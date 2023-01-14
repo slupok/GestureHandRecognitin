c43e65581b5a0861a3a1aec97e8580aeaa268aa9
Maksim <slupoke@yandex.ru>
2023-01-14 21:44:56 +0300

    GHR-1: Сборка с Cmake
    - закоментированны пути
    
    Branch:  master
    Revision:  17

--- cmake_generate.bat
+++ cmake_generate.bat
@@ -1,5 +1,5 @@
-set PATH=%PATH%;C:/Qt/5.15.2/mingw81_64/bin;
-set PATH=%PATH%;C:/Qt/Tools/Cmake_64/bin; 
-set PATH=%PATH%;C:/Qt/Tools/mingw810_64/bin;
+rem set PATH=%PATH%;C:/Qt/5.15.2/mingw81_64/bin;
+rem set PATH=%PATH%;C:/Qt/Tools/Cmake_64/bin; 
+rem set PATH=%PATH%;C:/Qt/Tools/mingw810_64/bin;
 
 cmake -G "MinGW Makefiles" -B "build"
--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 cd %cd%/build/
-set PATH=%PATH%;C:/Qt/Tools/mingw810_64/bin;
+rem set PATH=%PATH%;C:/Qt/Tools/mingw810_64/bin;
 
 mingw32-make
